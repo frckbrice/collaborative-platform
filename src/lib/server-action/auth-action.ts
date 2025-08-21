e624da6d1f0469a3433ccadcf3c1eefb179efddd
AVOM  Brice <maebrie2017@gmail.com>
2025-08-21 09:27:41 +0100

    Update auth-action.ts

--- src/lib/server-action/auth-action.ts
+++ src/lib/server-action/auth-action.ts
@@ -83,7 +83,7 @@ export async function socialLogin(provider: 'google' | 'github') {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/callback`,
     },
   });
 
